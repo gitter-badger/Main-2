2ee5227354ca40d5caeea2c8a07a8ad984622c99
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-08-22 23:14:29 +0300

    Moved PlayerScore struct to Contracts and added time field

---
+++ src/BaloonsPop.Common/Contracts/PlayerScore.cs
@@ -0,0 +1,23 @@
+﻿namespace BaloonsPop.Common.Contracts
+{
+    using System;
+
+    public struct PlayerScore : IComparable<PlayerScore>
+    {
+        public string Name;
+        public int Moves;
+        public DateTime Time;
+
+        public PlayerScore(string name, int moves, DateTime time)
+        {
+            this.Name = name;
+            this.Moves = moves;
+            this.Time = time;
+        }
+
+        public int CompareTo(PlayerScore other)
+        {
+            return this.Moves.CompareTo(other.Moves);
+        }
+    }
+}
